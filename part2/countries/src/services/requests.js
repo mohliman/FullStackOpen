49d6ca1995b8122bf30c7eb144502d13e6eb6c03
Muhammad Baqir Abubakar <bankitliman@gmail.com>
2024-05-18 14:59:31 +0100

    update part 2

--- part2/countries/src/services/requests.js
+++ part2/countries/src/services/requests.js
@@ -1,12 +1,13 @@
 import axios from 'axios'
 
 const countriesURL = `https://studies.cs.helsinki.fi/restcountries/api/all`
-
+const api_key = import.meta.env.VITE_SOME_KEY
 const getWeatherData = (city) => {
     return axios
-        .get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=aab09c47ff6a22bd71d49353cc554e0b`)
+        .get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${api_key}`)
         .then(response => response)
 }
+
 
 const getAll = () => {
     return axios
